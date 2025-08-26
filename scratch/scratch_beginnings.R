58a5ba7221b1ac0dc940a8c4bd4f754fd83ece1c
zachyyy700 <zachyyy700@gmail.com>
2025-08-26 10:53:29 -0700

    joining dfs

--- scratch/scratch_beginnings.R
+++ scratch/scratch_beginnings.R
@@ -57,8 +57,70 @@ bq1_k_df <- bq1_df |>
 
 # sma im going to cry
 bq1_k_df <- bq1_k_df |> 
-  mutate(sma_k = rollmean(k, k = 9, fill = NA))
+  filter(year <= 1994) |> 
+  mutate(sma_k_bq1 = rollmean(k, k = 9, fill = NA))
 
-ggplot(bq1_k_df) + 
+ggplot(prm_k_df) + 
   geom_line(aes(x = sample_date, y = k), color = "blue") +
-  geom_line(aes(x = sample_date, y = sma_k), color = "red")
+  geom_line(aes(x = sample_date, y = sma_k_prm), color = "red")
+
+# plot 'looks' correct. try to extend to other streams, same parameter
+bq2_k_df <- bq2_df |> 
+  mutate(year = year(sample_date)) |> 
+  mutate(week = week(sample_date)) |> 
+  mutate(day = day(sample_date)) |> 
+  select(sample_date, k, year, week, day)
+
+bq2_k_df <- bq2_k_df |> 
+  filter(year <= 1994) |> 
+  mutate(sma_k_bq2 = rollmean(k, k = 9, fill = NA))
+
+# bq3
+bq3_k_df <- bq3_df |> 
+  mutate(year = year(sample_date)) |> 
+  mutate(week = week(sample_date)) |> 
+  mutate(day = day(sample_date)) |> 
+  select(sample_date, k, year, week, day)
+
+bq3_k_df <- bq3_k_df |> 
+  filter(year <= 1994) |> 
+  mutate(sma_k_bq3 = rollmean(k, k = 9, fill = NA))
+
+#prm
+prm_k_df <- prm_df |> 
+  mutate(year = year(sample_date)) |> 
+  mutate(week = week(sample_date)) |> 
+  mutate(day = day(sample_date)) |> 
+  select(sample_date, k, year, week, day)
+
+prm_k_df <- prm_k_df |> 
+  filter(year <= 1994) |> 
+  mutate(sma_k_prm = rollmean(k, k = 9, fill = NA))
+
+# join
+all_streams_k <- left_join(bq1_k_df, bq2_k_df, by = "sample_date") 
+all_streams_k <- left_join(all_streams_k, bq3_k_df, by = "sample_date")
+all_streams_k <- left_join(all_streams_k, prm_k_df, by = "sample_date")
+
+ggplot(all_streams_k, aes(sample_date)) +
+  geom_line(aes(y = sma_k_bq1), color = "blue") +
+  geom_line(aes(y = sma_k_bq2), color = "darkgreen") +
+  geom_line(aes(y = sma_k_bq3), color = "red") +
+  geom_line(aes(y = sma_k_prm), color = "magenta")
+
+# need longer df. have column for stream name.
+# i want to combine my four dfs. pivot longer, and have a new col for stream name. leave parameter cols.
+# what to join by? 
+# also, all four dfs are different lengths
+
+#bq1 & bq2 section
+# use ends_with()!
+all_streams <- full_join(bq1_df, bq2_df, by = "sample_date") |> 
+  rename(k_bq1 = "k.x", no3_n_bq1 = "no3_n.x", mg_bq1 = "mg.x", ca_bq1 = "ca.x", nh4_n_bq1 = "nh4_n.x",
+         k_bq2 = "k.y", no3_nbq2 = "no3_n.y", mg_bq2 = "mg.y", ca_bq2 = "ca.y", nh4_n_bq2 = "nh4_n.y") |> 
+  select(sample_date, k_bq1, no3_n_bq1, mg_bq1, ca_bq1, nh4_n_bq1, 
+         k_bq2, no3_nbq2, mg_bq2, ca_bq2, nh4_n_bq2)
+  
+
+
+all_streams <- full_join(all_streams, )
