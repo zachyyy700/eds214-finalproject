6cc25cd556d087f58d4d63b38a4cff2c5bacdb9e
zachyyy700 <zachyyy700@gmail.com>
2025-08-26 21:50:19 -0700

    more scratching

--- scratch/scratch_beginnings.R
+++ scratch/scratch_beginnings.R
@@ -1,11 +1,22 @@
-## scratching spaghetti
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##                                                                            ~~
+##                            SCRATCHING SPAGHETTI                          ----
+##                                                                            ~~
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Purpose: The R script serves as spaghetti for the EDS214 Final Project. As spaghetti, this script will be the birth place for any final documents that will be created in docs folder after tinkering with code is finished here.
+## Author: Zachary Loo
+## Email: zachyyy700@gmail.com
 
 pacman::p_load("tidyverse", 
                "forecast",
                "lubridate",
-               "zoo")
+               "zoo",
+               "patchwork",
+               "slider")
 # read in data
-swapped_df <- read_csv(here::here("data", "QuebradaCuenca1-Bisley.csv")) |> 
+bq1_df <- read_csv(here::here("data", "QuebradaCuenca1-Bisley.csv")) |> 
   janitor::clean_names()
 bq2_df <- read_csv(here::here("data", "QuebradaCuenca2-Bisley.csv")) |> 
   janitor::clean_names()
@@ -109,18 +120,192 @@ ggplot(all_streams_k, aes(sample_date)) +
   geom_line(aes(y = sma_k_prm), color = "magenta")
 
 # need longer df. have column for stream name.
-# i want to combine my four dfs. pivot longer, and have a new col for stream name. leave parameter cols.
-# what to join by? 
-# also, all four dfs are different lengths
+# i want to combine my four dfs. leave parameter cols.
+# join by date
+# also, all four dfs are different lengths ?
 
 #bq1 & bq2 section
 # use ends_with()!
-all_streams <- full_join(bq1_df, bq2_df, by = "sample_date") |> 
+all_streams <- full_join(swapped_df, bq2_df, by = "sample_date") |> 
   rename(k_bq1 = "k.x", no3_n_bq1 = "no3_n.x", mg_bq1 = "mg.x", ca_bq1 = "ca.x", nh4_n_bq1 = "nh4_n.x",
-         k_bq2 = "k.y", no3_nbq2 = "no3_n.y", mg_bq2 = "mg.y", ca_bq2 = "ca.y", nh4_n_bq2 = "nh4_n.y") |> 
-  select(sample_date, k_bq1, no3_n_bq1, mg_bq1, ca_bq1, nh4_n_bq1, 
-         k_bq2, no3_nbq2, mg_bq2, ca_bq2, nh4_n_bq2)
+         k_bq2 = "k.y", no3_nbq2 = "no3_n.y", mg_bq2 = "mg.y", ca_bq2 = "ca.y", nh4_n_bq2 = "nh4_n.y") 
   
+# join in bq3
+all_streams <- full_join(all_streams, bq3_df, by = "sample_date")
 
+all_streams <- rename(all_streams, no3_n_bq3 = "no3_n", mg_bq3 = "mg", ca_bq3 = "ca", na4_n_bq3 = "nh4_n") 
 
-all_streams <- full_join(all_streams, )
+all_streams <- all_streams |> 
+  select(sample_date, ends_with(c("bq1", "bq2", "bq3")))
+
+# naming is a pain,
+# trying bind_rows()
+binded <- bind_rows(bq1_df, bq2_df)
+binded <- bind_rows(binded, bq3_df)  
+binded <- bind_rows(binded, prm_df)
+
+# this looks good, but how to mutate() the sma into it?
+binded3 <- binded3 |> 
+  filter(sample_date <= "1994-01-01") |> 
+  select(sample_date, sample_id, k, no3_n, mg, ca, nh4_n) |> 
+  mutate(sma_k = case_when(sample_id == "Q1" ~ rollmean(k, k = 9, fill = NA),
+                           sample_id == "Q2" ~ rollmean(k, k = 9, fill = NA),
+                           sample_id == "Q3" ~ rollmean(k, k = 9, fill = NA),
+                           sample_id == "MPR" ~ rollmean(k, k = 9, fill = NA)))
+
+p1 <- ggplot(binded3, aes(x = sample_date, y = sma_k, color = sample_id)) +
+  geom_line()
+
+# sma_no3_n
+binded3 <- binded3 |> 
+  mutate(sma_no3_n = case_when(sample_id == "Q1" ~ rollmean(no3_n, k = 9, fill = NA),
+                               sample_id == "Q2" ~ rollmean(no3_n, k = 9, fill = NA),
+                               sample_id == "Q3" ~ rollmean(no3_n, k = 9, fill = NA),
+                               sample_id == "MPR" ~ rollmean(no3_n, k = 9, fill = NA)))
+
+p2 <- ggplot(binded3, aes(x = sample_date, y = sma_no3_n, color = sample_id)) +
+  geom_line()
+
+# mg
+binded3 <- binded3 |> 
+  mutate(sma_mg = case_when(sample_id == "Q1" ~ rollmean(mg, k = 9, fill = NA),
+                               sample_id == "Q2" ~ rollmean(mg, k = 9, fill = NA),
+                               sample_id == "Q3" ~ rollmean(mg, k = 9, fill = NA),
+                               sample_id == "MPR" ~ rollmean(mg, k = 9, fill = NA)))
+
+p3 <- ggplot(binded3, aes(x = sample_date, y = sma_mg, color = sample_id)) +
+  geom_line()
+
+# ca 
+binded3 <- binded3 |> 
+  mutate(sma_ca = case_when(sample_id == "Q1" ~ rollmean(ca, k = 9, fill = NA),
+                            sample_id == "Q2" ~ rollmean(ca, k = 9, fill = NA),
+                            sample_id == "Q3" ~ rollmean(ca, k = 9, fill = NA),
+                            sample_id == "MPR" ~ rollmean(ca, k = 9, fill = NA)))
+
+p4 <- ggplot(binded3, aes(x = sample_date, y = sma_ca, color = sample_id)) +
+  geom_line()
+
+# nh4_n
+binded3 <- binded3 |> 
+  mutate(sma_nh4_n = case_when(sample_id == "Q1" ~ rollmean(nh4_n, k = 9, fill = NA),
+                            sample_id == "Q2" ~ rollmean(nh4_n, k = 9, fill = NA),
+                            sample_id == "Q3" ~ rollmean(nh4_n, k = 9, fill = NA),
+                            sample_id == "MPR" ~ rollmean(nh4_n, k = 9, fill = NA)))
+
+p5 <- ggplot(binded3, aes(x = sample_date, y = sma_nh4_n, color = sample_id)) +
+  geom_line()
+
+# combined final plot (maybe, probably not)
+final_plot <- (p1 / p2 / p3 / p4 / p5)
+
+final_plot *
+  theme_light()
+
+# na values are too plentiful
+#testing slide()
+rm(list = ls())
+
+rm(binded_slide)
+
+binded_slide <- binded |> 
+  filter(sample_date <= "1994-01-01") |> 
+  select(sample_date, sample_id, k, no3_n, mg, ca, nh4_n) |> 
+  slice_head(n = 30)
+
+# want slide_index_dbl() for 'awareness' of sample_date col
+# might need .complete = FALSE arguement to allow partial windows at edges
+binded_slide <- binded_slide |> 
+  mutate(
+    roll_k = slide_index_dbl(.x = k, .i = sample_date, .f = mean, .before = weeks(4), .after = weeks(4), .complete = FALSE)
+    )
+
+# i think slider method is better, let's try the whole df now
+# can definitely think of a for loop for this
+# sample_date must be in ascending order according to slider documentation
+test_all <- binded |> 
+  filter(sample_date <= "1994-01-01") |> 
+  select(sample_date, sample_id, k, no3_n, mg, ca, nh4_n) 
+
+# k to roll_k etc.
+test_all_q1 <- test_all |> 
+  filter(sample_id == "Q1") |> 
+  mutate(
+    roll_k = slide_index_dbl(.x = k, .i = sample_date, .f = ~mean(.x, na.rm = TRUE), .before = weeks(4), .after = weeks(4), .complete = FALSE),
+    roll_no3_n = slide_index_dbl(.x = no3_n, .i = sample_date, .f = ~mean(.x, na.rm = TRUE), .before = weeks(4), .after = weeks(4), .complete = FALSE),
+    roll_mg = slide_index_dbl(.x = mg, .i = sample_date, .f = ~mean(.x, na.rm = TRUE), .before = weeks(4), .after = weeks(4), .complete = FALSE),
+    roll_ca = slide_index_dbl(.x = ca, .i = sample_date, .f = ~mean(.x, na.rm = TRUE), .before = weeks(4), .after = weeks(4), .complete = FALSE),
+    roll_nh4_n = slide_index_dbl(.x = nh4_n, .i = sample_date, .f = ~mean(.x, na.rm = TRUE), .before = weeks(4), .after = weeks(4), .complete = FALSE)
+  )
+
+test_all_q2 <- test_all |> 
+  filter(sample_id == "Q2") |> 
+  mutate(
+    roll_k = slide_index_dbl(.x = k, .i = sample_date, .f = ~mean(.x, na.rm = TRUE), .before = weeks(4), .after = weeks(4), .complete = FALSE),
+    roll_no3_n = slide_index_dbl(.x = no3_n, .i = sample_date, .f = ~mean(.x, na.rm = TRUE), .before = weeks(4), .after = weeks(4), .complete = FALSE),
+    roll_mg = slide_index_dbl(.x = mg, .i = sample_date, .f = ~mean(.x, na.rm = TRUE), .before = weeks(4), .after = weeks(4), .complete = FALSE),
+    roll_ca = slide_index_dbl(.x = ca, .i = sample_date, .f = ~mean(.x, na.rm = TRUE), .before = weeks(4), .after = weeks(4), .complete = FALSE),
+    roll_nh4_n = slide_index_dbl(.x = nh4_n, .i = sample_date, .f = ~mean(.x, na.rm = TRUE), .before = weeks(4), .after = weeks(4), .complete = FALSE)
+  )
+
+test_all_q3 <- test_all |> 
+  filter(sample_id == "Q3") |> 
+  mutate(
+    roll_k = slide_index_dbl(.x = k, .i = sample_date, .f = ~mean(.x, na.rm = TRUE), .before = weeks(4), .after = weeks(4), .complete = FALSE),
+    roll_no3_n = slide_index_dbl(.x = no3_n, .i = sample_date, .f = ~mean(.x, na.rm = TRUE), .before = weeks(4), .after = weeks(4), .complete = FALSE),
+    roll_mg = slide_index_dbl(.x = mg, .i = sample_date, .f = ~mean(.x, na.rm = TRUE), .before = weeks(4), .after = weeks(4), .complete = FALSE),
+    roll_ca = slide_index_dbl(.x = ca, .i = sample_date, .f = ~mean(.x, na.rm = TRUE), .before = weeks(4), .after = weeks(4), .complete = FALSE),
+    roll_nh4_n = slide_index_dbl(.x = nh4_n, .i = sample_date, .f = ~mean(.x, na.rm = TRUE), .before = weeks(4), .after = weeks(4), .complete = FALSE)
+  )
+
+test_all_prm <- test_all |> 
+  filter(sample_id == "MPR") |> 
+  mutate(
+    roll_k = slide_index_dbl(.x = k, .i = sample_date, .f = ~mean(.x, na.rm = TRUE), .before = weeks(4), .after = weeks(4), .complete = FALSE),
+    roll_no3_n = slide_index_dbl(.x = no3_n, .i = sample_date, .f = ~mean(.x, na.rm = TRUE), .before = weeks(4), .after = weeks(4), .complete = FALSE),
+    roll_mg = slide_index_dbl(.x = mg, .i = sample_date, .f = ~mean(.x, na.rm = TRUE), .before = weeks(4), .after = weeks(4), .complete = FALSE),
+    roll_ca = slide_index_dbl(.x = ca, .i = sample_date, .f = ~mean(.x, na.rm = TRUE), .before = weeks(4), .after = weeks(4), .complete = FALSE),
+    roll_nh4_n = slide_index_dbl(.x = nh4_n, .i = sample_date, .f = ~mean(.x, na.rm = TRUE), .before = weeks(4), .after = weeks(4), .complete = FALSE)
+  )
+
+# bind rows
+all_means_and_streams <- bind_rows(test_all_q1, test_all_q2, test_all_q3, test_all_prm)
+
+p1 <- ggplot(all_means_and_streams, aes(sample_date)) +
+  geom_line(aes(y = roll_k, color = sample_id)) +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank())
+p1
+
+p2 <- ggplot(all_means_and_streams, aes(sample_date)) +
+  geom_line(aes(y = roll_no3_n, color = sample_id)) +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank())
+p2
+
+p3 <- ggplot(all_means_and_streams, aes(sample_date)) +
+  geom_line(aes(y = roll_mg, color = sample_id)) +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank())
+p3
+
+p4 <- ggplot(all_means_and_streams, aes(sample_date)) +
+  geom_line(aes(y = roll_ca, color = sample_id)) +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank())
+p4
+
+p5 <- ggplot(all_means_and_streams, aes(sample_date)) +
+  geom_line(aes(y = roll_nh4_n, color = sample_id)) 
+p5
+
+p_all <- p1 / p2 / p3 / p4 / p5
+p_all
+# attempt using across() for repeating slide_index_dbl() calls
+#new_names <- c("roll_k", "roll_no3_n", "roll_mg", "roll_ca", "roll_nh4_n")
+# test_all_q1 |> 
+#   mutate(across(k:nh4_n), slide_index_dbl(.i = sample_date, .f = mean, .before = weeks(4), .after = weeks(4), .complete = FALSE), .names = "roll_{.col}")
