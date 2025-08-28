f5951c5b8aba4398d618bbefa41c4a36197e22f7
zachyyy700 <zachyyy700@gmail.com>
2025-08-28 11:07:10 -0700

    update page

--- R/moving_avg_function.R
+++ R/moving_avg_function.R
@@ -1,21 +1,17 @@
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##                                                                            ~~
-##                  FUNCTION FOR CALCULATING MOVING AVERAGES                ----
-##                                                                            ~~
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## Purpose: The R script serves as the source function for the final quarto document.
+## Purpose: The R script serves as the moving average function for the final quarto document. To calculate the 9-week moving average, a function was created that accepts four parameters: `focal_date`, `dates`, `conc`, and `window_size_wks` which was kept at `window_size_wks` = 9.
 ## Author: Zachary Loo
 ## Email: zachyyy700@gmail.com
 
+# To handle the irregular size of each window when calculating each moving average (multiple observations in one week or large gaps between observations), the first step of the function is to create the appropriate window.
 moving_average <- function(focal_date, dates, conc, window_size_wks) {
-  # Steps for calculating moving avg over single focal date
+  
   # 1. Which dates are in the window?
   is_in_window <- (dates > focal_date - (window_size_wks / 2) * 7) & 
     (dates < focal_date + (window_size_wks / 2) * 7)
+  
   # 2. Find associated concentrations
   window_conc <- conc[is_in_window]
+  
   # 3. Calculate mean
   result <- mean(window_conc, na.rm = TRUE) #need na.rm
   
--- R/zapply.R
+++ R/zapply.R
@@ -1,15 +1,9 @@
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##                                                                            ~~
-##                  FUNCTION FOR APPLYING SAPPLY()                          ----
-##                                                                            ~~
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## Purpose: The R script serves as the another source function for the final quarto document.
+## Purpose: The R script serves to streamline the creation of new columns, calculate the moving average for each, and create the new respective columns.
+
 ## Author: Zachary Loo
 ## Email: zachyyy700@gmail.com
 
-# for now don't worry about sample_id
+# for now don't worry about sample_id, will be handled later
 zapply <- function(df, start_col, end_col, func, window_size_wks = 9) {
   
   for (i in start_col:end_col) {
